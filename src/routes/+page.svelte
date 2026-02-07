71e6c7535558fab83f9f401dfbd36e750916dbeb
Mitchel Rojas <mrojasr@usm.cl>
2026-02-07 11:48:22 -0300

    feat: add layout and about pages with initial structure and styles

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Main from '$lib/components/Main.svelte';
+</script>
+
+<svelte:head>
+	<title>Mitchel Rojas</title>
+	<meta name="description" content="Avocado a GNU/Linux y el Open Source. Estudiante de Ingeniería." />
+</svelte:head>
+
+<Main />
