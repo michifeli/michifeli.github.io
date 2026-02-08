dbd1eb1fe7205ebcc04b1e3c01f19ca8a772e2ac
Mitchel Rojas <mrojasr@usm.cl>
2026-02-08 09:20:34 -0300

    feat: update svelte.config.js to support .md files

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,17 @@ import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+
+	preprocess: [
+		mdsvex({
+			extensions: ['.md']
+		})
+	],
+
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
-	},
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	}
 };
 
 export default config;
