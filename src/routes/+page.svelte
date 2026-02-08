083234d1afffc40fc276f04ba97d730d1a0bb6c0
Mitchel Rojas <mrojasr@usm.cl>
2026-02-08 09:22:55 -0300

    feat: update +page.svelte to pass posts data to Main component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Main from '$lib/components/Main.svelte';
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
 </script>
 
 <svelte:head>
 	<title>Mitchel Rojas</title>
-	<meta name="description" content="Avocado a GNU/Linux y el Open Source. Estudiante de Ingeniería." />
 </svelte:head>
 
-<Main />
+<Main posts={data.posts} />
